2bba0c89630ad4d3cf998a5b4d8a4d946b1d64c7
Andrii Savchuk <andriksr@gmail.com>
2018-05-22 22:25:49 +0200

    add signup for user

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const router  = express.Router();
+const express   = require('express');
+const router    = express.Router();
+const gravatar  = require('gravatar');
+const bcrypt    = require('bcryptjs');
 
 // Load User model
 const User = require('../../models/User');
@@ -13,8 +15,38 @@ router.get('/', (req, res) => res.json({
 // @route  POST api/user
 // @desc   User registration
 // @acces  Public
-router.post('/signup', (req, res, next) => res.json({
-  
-}));
+router.post('/signup', (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        return res.status(409).json({ email: 'User with current email already exists.' })
+      } else {
+        const avatar = gravatar.url(req.body.email, {
+          s: 200,  // Size
+          r: 'pg', // Rating
+          d: 'mm'  // Default
+        });
+
+        const newUser = new User({
+          name: req.body.name,
+          surname: req.body.surname,
+          email: req.body.email,
+          avatar,
+          password: req.body.password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          })
+        })
+      }
+    })
+});
 
 module.exports = router;
