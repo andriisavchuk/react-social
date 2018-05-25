9107c390a73d9f69169bf1404dc46f647cfd3d0e
Andrii Savchuk <andriksr@gmail.com>
2018-05-25 11:41:00 +0200

    begin work with validation

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,55 +1,67 @@
-const express   = require('express');
-const router    = express.Router();
-const gravatar  = require('gravatar');
-const bcrypt    = require('bcryptjs');
-const jwt       = require('jsonwebtoken');
-const keys      = require('../../config/keys');
-const passport  = require('passport');
+const express = require('express');
+const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
+
+// Load input validation
+const validateSignupInput = require('../../validation/signup');
 
 // Load User model
 const User = require('../../models/User');
 
 // @route  GET api/user
 // @acces  Public
-router.get('/', (req, res) => res.json({
-  message: "Hello from user router"
-}));
+router.get('/', (req, res) =>
+  res.json({
+    message: 'Hello from user router'
+  })
+);
 
 // @route  POST api/user/signup
 // @desc   User registration
 // @acces  Public
 router.post('/signup', (req, res, next) => {
-  User.findOne({ email: req.body.email })
-    .then(user => {
-      if (user) {
-        return res.status(409).json({ email: 'User with current email already exists.' })
-      } else {
-        const avatar = gravatar.url(req.body.email, {
-          s: 200,  // Size
-          r: 'pg', // Rating
-          d: 'mm'  // Default
-        });
+  const { errors, isValid } = validateSignupInput(req.body);
 
-        const newUser = new User({
-          name: req.body.name,
-          surname: req.body.surname,
-          email: req.body.email,
-          avatar,
-          password: req.body.password
-        });
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
 
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-            newUser.password = hash;
-            newUser
-              .save()
-              .then(user => res.json(user))
-              .catch(err => console.log(err));
-          })
-        })
-      }
-    })
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (user) {
+      errors.email = 'User with current email already exists.';
+      return res.status(409).json(errors);
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: 200, // Size
+        r: 'pg', // Rating
+        d: 'mm' // Default
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        surname: req.body.surname,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then((user) => res.json(user))
+            .catch((err) => console.log(err));
+        });
+      });
+    }
+  });
 });
 
 // @route  POST api/user/login
@@ -60,44 +72,56 @@ router.post('/login', (req, res, next) => {
   const password = req.body.password;
 
   //Find user by email
-  User.findOne({ email })
-    .then(user => {
-      //Check for user
-      if (!user) {
-        return res.status(404).json({ email: 'User not found' });
-      }
+  User.findOne({ email }).then((user) => {
+    //Check for user
+    if (!user) {
+      return res.status(404).json({ email: 'User not found' });
+    }
 
-      // Check password
-      bcrypt.compare(password, user.password)
-        .then(isMatch => {
-          if (isMatch) {
-            // User matched            
-            const payload = { id: user.id, name: user.name, surname: user.surname, avatar: user.avatar }
-            
-            // Sign Token
-            jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
-              res.json({
-                success: true,
-                token: 'Bearer ' + token
-              });
+    // Check password
+    bcrypt.compare(password, user.password).then((isMatch) => {
+      if (isMatch) {
+        // User matched
+        const payload = {
+          id: user.id,
+          name: user.name,
+          surname: user.surname,
+          avatar: user.avatar
+        };
+
+        // Sign Token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: 'Bearer ' + token
             });
-          } else {
-            return res.status(400).json({ password: 'Password is incorrect' });
           }
-        })
-    })
+        );
+      } else {
+        return res.status(400).json({ password: 'Password is incorrect' });
+      }
+    });
+  });
 });
 
 // @route  GET api/user/current
 // @desc   Return current user
 // @acces  Private
-router.get('/current', passport.authenticate('jwt', { session: false }), (req, res, next) => {
-  res.json({
-    id: req.user.id,
-    name: req.user.name,
-    surname: req.user.surname,
-    email: req.user.email,
-  });
-})
+router.get(
+  '/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res, next) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      surname: req.user.surname,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
--- validation/signup.js
+++ validation/signup.js
@@ -1,14 +1,15 @@
 const Validator = require('validator');
+const isEmpty = require('./isEmpty');
 
 module.exports = function(data) {
   let errors = {};
 
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
-    errors.name = 'Nmae must be between 2 and 30 characters';
+    errors.name = 'Name must be between 2 and 30 characters';
   }
 
   return {
     errors,
-    isValid: errors
+    isValid: isEmpty(errors)
   };
 };
