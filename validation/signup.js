f5db560dbefcf5776d74c700b27b157a0a0dde32
Andrii Savchuk <andriksr@gmail.com>
2018-05-29 10:05:31 +0200

    finished setup a validator for signup logic

--- validation/signup.js
+++ validation/signup.js
@@ -4,8 +4,41 @@ const isEmpty = require('./isEmpty');
 module.exports = function(data) {
   let errors = {};
 
+  data.name = !isEmpty(data.name) ? data.name : '';
+  data.email = !isEmpty(data.email) ? data.email : '';
+  data.password = !isEmpty(data.password) ? data.password : '';
+  data.conf_password = !isEmpty(data.conf_password) ? data.conf_password : '';
+
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = 'Name must be between 2 and 30 characters';
+  }
+
+  if (Validator.isEmpty(data.name)) {
+    errors.name = 'Name field is required';
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = 'Email field is required';
+  }
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = 'Email format is invalid';
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = 'Password is required';
+  }
+
+  if (!Validator.isLength(data.password), {min: 6, max: 30}) {
+    errors.password = 'Password must be at least 6 characters';
+  }
+
+  if (Validator.isEmpty(data.conf_password)) {
+    errors.conf_password = 'Confirm password is required';
+  }
+
+  if (!Validator.equals(data.password, data.conf_password)) {
+    errors.conf_password = 'Password must match';
   }
 
   return {
