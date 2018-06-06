3cfd0d72ec38d7ec476cab2622f540c32f263fbf
Andrii Savchuk <andriksr@gmail.com>
2018-06-06 17:41:05 -0500

    add populate methot to get info about user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -19,14 +19,15 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
   const errors = {};
 
   Profile.findOne({ user: req.user.id })
-    .then(profile => {
-      if (!profile) {
-        errors.noprofile = 'There is no profile for such user';
-        return res.status(400).json(errors);
-      }
-      res.json(profile);
-    })
-    .catch(err => res.status(404).json(err));
+    .populate('user', ['name', 'avatar'])
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = 'There is no profile for such user';
+          return res.status(400).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
 });
 
 // @route  POST api/profile
