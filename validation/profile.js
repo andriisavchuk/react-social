1034ab4a052564b03875a78fc5a03671ba7c8260
Andrii Savchuk <andriksr@gmail.com>
2018-06-06 16:23:03 -0500

    fix

---
+++ validation/profile.js
@@ -0,0 +1,17 @@
+const Validator = require('validator');
+const isEmpty   = require('./isEmpty');
+
+module.exports = function validateProfileInput(data) {
+  let errors = {};
+
+  data.handle = !isEmpty(data.handle) ? data.handle : '';
+  data.status = !isEmpty(data.status) ? data.status : '';
+  data.skills = !isEmpty(data.skills) ? data.skills : '';
+
+
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
