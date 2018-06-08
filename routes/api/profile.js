a0004e2e78ea42e0512a7eb306788398adba4fc0
Andrii Savchuk <andriksr@gmail.com>
2018-06-08 18:52:33 -0500

    added validation for experience route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 
 // Load input validation
 const validateProfileInput = require('../../validation/profile');
+const validateExperienceInput = require('../../validation/experience');
 
 // Load Profile Model
 const Profile = require('../../models/Profile');
@@ -153,6 +154,14 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
 // @desc   Add experience user profile
 // @acces  Private
 router.post('/experience', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const { errors, isValid } = validateExperienceInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    // return any errors with 400 status
+    return res.status(400).json(errors);
+  }
+
   Profile.findOne({ user: req.user.id })
     .then(profile => {
       const newExperience = {
