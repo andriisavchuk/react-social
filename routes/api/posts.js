3a0130b1510423072143f7c3a6784cf96f84daef
Andrii Savchuk <andriksr@gmail.com>
2018-06-11 16:46:09 -0500

    create post request to add new posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,29 @@
-const express = require('express');
-const router  = express.Router();
+const express  = require('express');
+const router   = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const Post     = require('../../models/Post');
 
 // @route  GET api/posts
 // @acces  Public
 router.get('/', (req, res) => res.json({
   message : "Hello from posts router"
 }));
+
+// @route  POST api/posts
+// @desc   Create post
+// @acces  Private
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const newPost = new Post({
+    text: req.body.text,
+    name: req.body.name,
+    avatar: req.body.avatar,
+    user: req.user.id
+  });
+
+  newPost.save().then(post => res.json(post));
+});
+
 
 module.exports = router;
