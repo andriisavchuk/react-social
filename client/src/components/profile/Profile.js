bd88cbfe2d1fcfc602569d559a2dd916a726403b
Andrii Savchuk <andriksr@gmail.com>
2018-07-15 12:34:35 -0500

    add getProfileByHandle action

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,23 +1,35 @@
-import React, { Component } from 'react',
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import ProfileHeader from './ProfileHeader',
-import ProfileAbout from './ProfileAbout',
-import ProfileCreds from './ProfileCreds',
+import ProfileHeader from './ProfileHeader';
+import ProfileAbout from './ProfileAbout';
+import ProfileCreds from './ProfileCreds';
+import ProfileGithub from './ProfileGithub';
 import Spinner from '../common/Spinner';
+import { getProfileByHandle } from '../../actions/profileActions';
 
 class Profile extends Component {
+  componentDidMount() {
+    if (this.props.match.params.handle) {
+      this.props.getProfileByHandle(this.props.match.params.handle);
+    }
+  }
+
   render() {
     return (
       <div>
-
+        <ProfileHeader />
+        <ProfileAbout />
+        <ProfileCreds />
+        <ProfileGithub />
       </div>
     )
   }
 }
 
 Profile.propTypes = {
+  getProfileByHandle: PropTypes.func.isRequired,
   profile: PropTypes.object.isRequired
 }
 
@@ -25,4 +37,4 @@ const mapStateToProps = state => ({
   profile: state.profile
 })
 
-export default connect(mapStateToProps)(Profile);
+export default connect(mapStateToProps, {getProfileByHandle})(Profile);
