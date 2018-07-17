622fb6988229d2a87fbebc69fd519b3be18be1f7
Andrii Savchuk <andriksr@gmail.com>
2018-07-17 12:14:43 -0500

    create ProfileAbout component

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import isEmpty from '../../validation/isempty';
 
 class ProfileAbout extends Component {
   render() {
+    const { profile } = this.props;
+
+    // Skill list
+    const skills = profile.skills.map((skill, index) => (
+      <div key={index} className="p-3">
+        <i className="fa fa-check" />
+        {skill}
+      </div>
+    ));
+
     return (
-      <div>
-        <h1>Porfile About</h1>
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card card-body bg-light mb-3">
+            <h3 className="text-center text-info">{profile.user.name} Bio</h3>
+            <p className="lead">
+              {isEmpty(profile.bio) ? (
+                <span>{profile.name} does not have a bio.</span>
+              ) : (
+                <span>{profile.bio}</span>
+              )}
+            </p>
+            <hr />
+            <h3 className="text-center text-info">Skill Set</h3>
+            <div className="row">
+              <div className="d-flex flex-wrap justify-content-center align-items-center">
+                {skills}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
