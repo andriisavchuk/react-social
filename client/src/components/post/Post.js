dfbd5da663b6bdf6faeb310486fb937bb0e622ed
Andrii Savchuk <andriksr@gmail.com>
2018-07-24 12:09:41 -0500

    Display post

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import PostItem from '../posts/PostItem';
 import Spinner from '../common/Spinner';
-import { getPost} from '../../actions/postActions';
+import { getPost } from '../../actions/postActions';
 
 class Post extends Component {
   componentDidMount() {
@@ -10,22 +12,46 @@ class Post extends Component {
   }
 
   render() {
+    const { post, loading } = this.props.post;
+    let postContent;
+
+    if (post === null || loading || Object.keys(post).length === 0) {
+      postContent = <Spinner />;
+    } else {
+      postContent = (
+        <div>
+          <PostItem post={post} showActions={false} />
+        </div>
+      );
+    }
+
     return (
-      <div>
-        <h1>Post</h1>
+      <div className="post">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <Link to="/feed" className="btn btn-light mb-3">
+                Back To Feed
+              </Link>
+              {postContent}
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
 Post.propTypes = {
   getPost: PropTypes.func.isRequired,
   post: PropTypes.object.isRequired
-}
+};
 
 const mapStateToProps = state => ({
   post: state.post
-})
+});
 
-
-export default connect(mapStateToProps, { getPost })(Post);
+export default connect(
+  mapStateToProps,
+  { getPost }
+)(Post);
